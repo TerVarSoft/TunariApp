c849d349aa80449bdb42b6cafb9e00a61fc14e2d
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2016-03-27 12:04:03 -0400

    Error in production mode, duplicate key
    -Added sparse property
    - Comments/ configurations

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/tunariDB'
+    uri: 'mongodb://localhost/tunariDB-prodTest'
   },
   loggingOptions: {
   	logDir: './../log'
