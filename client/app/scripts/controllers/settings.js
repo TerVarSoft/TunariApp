2d78cbaee198d18e16741ef347df355a4744ee3d
Mirko Jhovanny Terrazas Vargas <mterrazas@pros.com>
2016-09-19 12:19:40 -0400

    Adde settings route

---
+++ client/app/scripts/controllers/settings.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:StatisticsCtrl
+ * @description
+ * # StatisticsCtrl
+ * Controller of the clientApp
+ */
+angular.module('tunariApp')
+  .controller('SettingsCtrl', ['$scope',  function ($scope, $uibModal, ServerData) {
+
+      $scope.layout.title = 'Configuraciones';
+     
+  }]);
